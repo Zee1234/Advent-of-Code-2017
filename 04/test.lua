6ba66086540b563ad4aa50fb12493a35d2645523
Zee1234 <zeeatgit@gmail.com>
2017-12-05 01:09:28 -0600

    It's off by one. Will attempt to figure out why tomorrow.

--- 04/test.lua
+++ 04/test.lua
@@ -4,10 +4,11 @@ function validatePassphrase(passphrase)
   if not passphrase:match(' ') then return false, passphrase, "\n\tSingle words are not allowed" end
   for word in passphrase:gmatch('%S+') do
     local count = 0
-    local match = passphrase:gmatch((' %s '):format(word))
+    -- local match = passphrase:gmatch((' %s '):format(word))
+    local match = passphrase:gmatch( ('[^%%a]?%s[^%%a]?'):format(word))
     while match() do count = count + 1 end
-    if passphrase:match('^'..word) then count = count + 1 end
-    if passphrase:match(word..'$') then count = count + 1 end
+    -- if passphrase:find('^'..word) then count = count + 1 end
+    -- if passphrase:find(word..'$') then count = count + 1 end
     if count > 1 then return false, passphrase, "\n\tDuplicate words" end
   end
   
@@ -16,9 +17,8 @@ end
 
 local i, j = 0, 0
 for line in io.lines 'list.txt' do
-  local res, string, err = validatePassphrase(line)
+  local res, string, err = validatePassphrase(line:gsub('\n', ''))
   if res then i = i + 1 end
-  --if not res then print(res, string, err) j = j + 1 end
-  if err and err ~= "\n\tDuplicate words" then print(res, string, err) j = j + 1 end
+  j = j + 1
 end
 print(i, j)
