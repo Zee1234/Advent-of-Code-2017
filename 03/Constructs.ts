4e0a2704d2f9d5e2b841b8b427b9e50f2f3b718c
Zee1234 <zeeatgit@gmail.com>
2017-12-05 22:12:52 -0600

    Additions to Grid

--- 03/Constructs.ts
+++ 03/Constructs.ts
@@ -34,15 +34,14 @@ export class Point extends Array<number> {
   }
 }
 
+
+
 let defaultProxy:ProxyHandler<ExtendableProxy> = {
   set: function(object: any, key, value, proxy) {
       object[key] = value;
       return true;
   }
 }
-
-
-
 
 export class ExtendableProxy {
   constructor(definition: ProxyHandler<any> = defaultProxy) {
@@ -66,17 +65,24 @@ interface SubGridProxy<T> extends ProxyHandler<SubGrid<T>> {
 class SubGrid<T> extends ExtendableProxy {
   [index: number]: T
   subgrid: {
-    [key: number]: T
+    [index: number]: T
   }
 
   constructor() {
     super({
       get: (target, property, reciever) => {
-        return target[property]
+        let p = Number.parseInt(property.toString())
+        if (isNaN(p)) {
+          return target[property]
+        } else {
+          target.subgrid = target.subgrid || {}
+          return target.subgrid[property]
+        }
       },
       set: (target, property, value, reciever) => {
+        target.subgrid = target.subgrid || {}
         try {
-          target[property] = value
+          target.subgrid[property] = value
         } catch(e) {
           return e
         }
@@ -86,7 +92,10 @@ class SubGrid<T> extends ExtendableProxy {
   }
 }
 
-
+interface subArray extends Array<number> {
+  1: number,
+  2?: number
+}
 type GridProxyGet<T> = (target: Grid<T>, property: number, reciever: Grid<T>) => SubGrid<T>
 interface GridProxy<T> extends ProxyHandler<Grid<T>> {
   get: GridProxyGet<T>
@@ -99,10 +108,53 @@ export class Grid<T> extends ExtendableProxy {
   constructor() {
     super({
       get: (target, property, reciever) => {
-        target.grid[property] = target.grid[property] || new SubGrid<T>()
-        return target.grid[property]
+        let p = Number.parseInt(property.toString())
+        if (isNaN(p)) {
+          return target[property]
+        } else {
+          target.grid[property] = target.grid[property] || new SubGrid<T>()
+          return target.grid[property]
+        }
       }
     } as GridProxy<T>)
     this.grid = {}
+  }
+
+  forEach(callback: (value: T, x?: number, y?: number, self?: Grid<T>) => void) {
+    Object.keys(this.grid).forEach( xs => {
+      let x = Number.parseInt(xs)
+      Object.keys(this[x].subgrid).forEach( ys => {
+        let y = Number.parseInt(ys)
+        callback(this[x][y], x, y, this)
+      })
+    })
+  }
+
+  reduce(callback: (accumulator: T, value: T, x?: number, y?: number, self?: Grid<T>) => T): T
+  reduce(callback: (accumulator: T, value: T, x?: number, y?: number, self?: Grid<T>) => T, accumulator: T): T
+  reduce<U>(callback: (accumulator: U, value: T, x?: number, y?: number, self?: Grid<T>) => U, accumulator: U=undefined): U {
+    this.forEach( (v, x, y, s) => {
+      accumulator = callback(accumulator, v, x, y, s)
+    })
+    return accumulator
+  }
+
+  sub([x]: [number], [y]: [number]): Grid<T>
+  sub([x1, x2]: [number, number], [y1, y2]: [number, number]): Grid<T>
+  sub(xArr: number[], yArr: number[]): Grid<T>{
+    let x1 = xArr[0]
+      , x2 = xArr[1] || x1
+      , y1 = yArr[0]
+      , y2 = yArr[1] || y1
+
+    let ret = new Grid<T>()
+    if (x2 < x1) [x1, x2] = [x2, x1]
+    if (y2 < y1) [y1, y2] = [y2, y1]
+    for (let x = x1; x <= x2; x+=1) {
+      for (let y = y1; y <= y2; y+=1) {
+        ret[x][y] = this[x][y]
+      }
+    }
+    return ret
   }
 }
