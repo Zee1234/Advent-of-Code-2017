03b262580477c08850687543592506956d195be3
Zee1234 <zeeatgit@gmail.com>
2017-12-17 22:27:20 -0600

    Improve Part 2

--- 17/Part2_2.ts
+++ 17/Part2_2.ts
@@ -1,13 +1,10 @@
 console.time('bruteForce')
 import { input as stepSize } from "./input";
-import { RepeatingList } from "../tsTypes/RepeatingList";
 
 let currentPos = 0
-  , length = 1
   , one = 0
 for (let i = 1; i <= 50000000; i++) {
-  currentPos = (currentPos+stepSize)%length+1
-  length++
+  currentPos = (currentPos+stepSize)%i+1
   if (currentPos===1) one = i
 }
 console.log(one)
